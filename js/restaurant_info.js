269229c85bf577b8c29a2f73de49e0fe760a0f92
Dennis Lin <lin.dennis18@gmail.com>
2018-08-11 11:06:13 -0700

    Responsive layout using flex

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZGVubmk1bGluIiwiYSI6ImNqa2swM2M4ZDFsdGUzcHFoODU3Y3ltbjgifQ.k3r2H0d5HLoFZEwwRT85MA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
